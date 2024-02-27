4cc3ce0e7c3617e6f9a32c4be5f5497a1960da70
Mehroj1202 <iphonexseria@gmail.com>
2024-02-27 14:59:20 +0500

    basic_function

--- basic_function_parameter02.py
+++ basic_function_parameter02.py
@@ -1,3 +1,8 @@
 # Create a function named calculate_sum that takes a list of numbers as a parameter.
 # Inside the function, calculate the sum of all the numbers in the given list.
 # Return the sum.
+def calculate_sum(a):
+    a=[1,2,3,4,5]
+    return sum(a)
+print(calculate_sum([12,4,2]))
+    
--- basic_function_parameter05.py
+++ basic_function_parameter05.py
@@ -1,3 +1,5 @@
 # Create a function named count_vowels that takes a string as a parameter.
 # Inside the function, count the number of vowels (a, e, i, o, u, A, E, I, O, U) in the given string.
 # Return the count of vowels.
+def count_vowels(a):
+    return a
